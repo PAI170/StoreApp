b991b2a0bb2fd8b509e522ded2d0172c2faaf46f
DESKTOP-7KB13SA\david <davidroddriguez896@gmail.com>
2024-05-22 15:06:09 -0600

    StoreDB: Se termino de crear las demas tablas pendientes.

--- StoreDB/StoreDB init.sql
+++ StoreDB/StoreDB init.sql
@@ -11,26 +11,74 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Products' and xtype = 'U')
 BEGIN
 	CREATE TABLE Products (
-		Product_id INT NOT NULL,
+		Product_Id INT IDENTITY (1,1) NOT NULL,
 		SKU INT NOT NULL,
-		Name VARCHAR(50) NOT NULL,
+		Product_Name VARCHAR(50) NOT NULL,
 		Price DECIMAL(10,2) NOT NULL,
 		Quantity INT NOT NULL,
-		CONSTRAINT PKProduct PRIMARY KEY (Product_id),
+		StateID INT NOT NULL,
+		CategoriesID INT NOT NULL,
+		CONSTRAINT PKProduct PRIMARY KEY (Product_Id),
 		CONSTRAINT UXSKU UNIQUE (SKU),
+		CONSTRAINT FKProductStateId FOREIGN KEY (StateID) REFERENCES States(Id),
+		CONSTRAINT FKProductCategoryID FOREIGN KEY (CategoriesID) REFERENCES Categories(Id)
 	)
 END
 GO
 
--- CREATE SHOPPING CART
-IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'ShoppingCart' and xtype = 'U')
+-- CREATE CATEGORIES
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Categories' and xtype = 'U')
 BEGIN
-	CREATE TABLE ShoppingCart (
-		ID INT NOT NULL,
-		SKU INT NOT NULL,
-		Quantity INT NOT NULL,
-		Subtotal DECIMAL(10,2) NOT NULL,
-		IVA DECIMAL(10,2) NOT NULL,
-		Total DECIMAL(10,2) NOT NULL,
-		CONSTRAINT PKID PRIMARY KEY (ID),
-)
+	CREATE TABLE Categories (
+	Id INT IDENTITY (1,1) NOT NULL,
+	Categories_Name VARCHAR(50) NOT NULL,
+	CONSTRAINT PKCategories PRIMARY KEY (Id)
+	)
+END
+GO
+
+-- CREATE STATES
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'States' and xtype = 'U')
+BEGIN
+	CREATE TABLE States (
+		Id INT IDENTITY (1,1) NOT NULL,
+		State_Name VARCHAR(50) NOT NULL,
+		CONSTRAINT PKState PRIMARY KEY (Id)
+	)
+END
+GO
+
+-- CREATE CUSTOMER
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Customer' and xtype = 'U')
+BEGIN
+	CREATE TABLE Customer (
+		Id INT IDENTITY (1,1) NOT NULL,
+		DocumentId INT NOT NULL, 
+		Name VARCHAR(50) NOT NULL,
+		LastName VARCHAR(50) NOT NULL,
+		PhoneNumber INT NOT NULL,
+		Country VARCHAR(50) NOT NULL,
+		State VARCHAR (50) NOT NULL,
+		City VARCHAR(50) NOT NULL,
+		Addres VARCHAR (50) NOT NULL,
+		CONSTRAINT PKDocumentId PRIMARY KEY (Id),
+		CONSTRAINT UXDocumentId UNIQUE (DocumentId)
+	)
+END
+GO 
+
+-- CREATE Completed Orders
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Orders' and xtype = 'U')
+BEGIN
+	CREATE TABLE Orders (
+		Id INT IDENTITY (1,1) NOT NULL,
+		Purchase_Order INT IDENTITY (1,1) NOT NULL,
+		Date DATETIME2(7) NOT NULL,
+		Product_Id INT NOT NULL,
+		Customer_Id INT NOT NULL,
+		CONSTRAINT PKOrdersId PRIMARY KEY (Id),
+		CONSTRAINT UXPurchaseOrder UNIQUE (Purchase_Order),
+		CONSTRAINT FKProductOrderId FOREIGN KEY (Product_Id) REFERENCES Products(Product_Id),
+		CONSTRAINT FKCustomerOrderId FOREIGN KEY (Customer_Id) REFERENCES Customer(Id)
+	)
+END
